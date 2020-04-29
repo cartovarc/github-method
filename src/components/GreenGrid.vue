ed1807ac6f34a0b1b8ced6d66b3d9313adce79bc
Carlos Tovar <cartovarc@gmail.com>
2020-04-29 09:30:57 -0500

    fix #8

--- src/components/GreenGrid.vue
+++ src/components/GreenGrid.vue
@@ -6,15 +6,36 @@
   >
     <svg class="squares" width="850" height="200">
       <g v-for="i in 7" v-bind:key="'c' + i">
-        <rect
-          v-for="j in TOTAL_WEEKS"
-          v-bind:key="j"
-          :x="(j - 1) * (BOX_SIZE + BOX_GUTTER) + 1"
-          :y="(i - 1) * (BOX_SIZE + BOX_GUTTER) + 1"
-          :width="BOX_SIZE"
-          :height="BOX_SIZE"
-          :style="'fill:' + getColor(i - 1, j - 1) + ';opacity:1'"
-        />
+        <g v-for="j in TOTAL_WEEKS" v-bind:key="j">
+          <rect
+            :x="(j - 1) * (BOX_SIZE + BOX_GUTTER) + 1"
+            :y="(i - 1) * (BOX_SIZE + BOX_GUTTER) + 1"
+            :width="BOX_SIZE"
+            :height="BOX_SIZE"
+            :style="'fill:' + getColor(i, j) + ';opacity:1'"
+          />
+          <text
+            v-if="debug"
+            :x="(j - 1) * (BOX_SIZE + BOX_GUTTER) + 1"
+            :y="(i - 1) * (BOX_SIZE + BOX_GUTTER) + 5"
+            font-family="Verdana"
+            font-size="5"
+            fill="blue"
+          >
+            {{ i }} - {{ j }}
+          </text>
+
+          <text
+            v-if="debug"
+            :x="(j - 1) * (BOX_SIZE + BOX_GUTTER) + 1"
+            :y="(i - 1) * (BOX_SIZE + BOX_GUTTER) + 10"
+            font-family="Verdana"
+            font-size="3"
+            fill="blue"
+          >
+            {{ getDateFromCoords(i, j) }}
+          </text>
+        </g>
       </g>
     </svg>
     <q-resize-observer @resize="onResize" />
@@ -32,6 +53,7 @@ const white = "#ffffff";
 const green_one = "#c6e48b";
 const green_two = "#7bc96f";
 const green_three = "#196127";
+const timeStamp = Date.now();
 
 export default {
   computed: {
@@ -39,36 +61,40 @@ export default {
     ...mapState("records", ["records", "todayRecords"]),
     allRecords() {
       return Object.assign({}, this.records, this.todayRecords);
+    },
+    iToday() {
+      return parseInt(date.formatDate(timeStamp, "d")) + 1;
+    },
+    jToday() {
+      return parseInt(this.TOTAL_WEEKS);
     }
   },
   data() {
     return {
       TOTAL_WEEKS: 52,
       BOX_SIZE: 13, //px
-      BOX_GUTTER: 3 //px
+      BOX_GUTTER: 3, //px
+      debug: false
     };
   },
   methods: {
     // i, j -> DDD-YYYY
     getDateFromCoords(i, j) {
-      let jToday = parseInt(this.TOTAL_WEEKS) - 1;
-      let timeStamp = Date.now();
-      let iToday = parseInt(date.formatDate(timeStamp, "d"));
       let todayDayOfYear = parseInt(date.formatDate(timeStamp, "DDD"));
       let todayYear = parseInt(date.formatDate(timeStamp, "YYYY"));
 
       let intermediateColums = 0;
-      if (jToday !== j && Math.abs(jToday - j) !== 1) {
-        intermediateColums = jToday - j - 1;
+      if (Math.abs(this.jToday - j) !== 1 && Math.abs(this.jToday - j) !== 0) {
+        intermediateColums = this.jToday - j - 1;
       }
 
-      let sameColum = jToday === j;
+      let sameColum = this.jToday === j;
       let distance = 0;
 
       if (sameColum) {
-        distance = iToday - i;
+        distance = this.iToday - i;
       } else {
-        distance = intermediateColums * 7 + iToday + (7 - i + 1);
+        distance = intermediateColums * 7 + this.iToday + (7 - i);
       }
 
       let DDDij = todayDayOfYear - distance;
@@ -81,13 +107,9 @@ export default {
     },
 
     getColor(i, j) {
-      let timeStamp = Date.now();
       let currentYear = parseInt(date.formatDate(timeStamp, "Y"));
-      let jToday = parseInt(this.TOTAL_WEEKS) - 1;
-      let iToday = parseInt(date.formatDate(timeStamp, "d"));
-
       // white in future days
-      if (i > iToday && j == jToday) {
+      if (i > this.iToday && j == this.jToday) {
         return white;
       }
 
@@ -106,7 +128,11 @@ export default {
       }
 
       if (totalContributions) {
-        return blend("#c6e48b", "#196127", normalize(totalContributions, 5, 0));
+        return blend(
+          "#c6e48b",
+          "#196127",
+          normalize(totalContributions, 5, 0) - 0.2
+        );
       }
 
       return gray;
